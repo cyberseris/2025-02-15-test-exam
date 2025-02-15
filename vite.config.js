4e021f892e666c3e9b8bca045d0e7004d499909c
Skey <chenskey8@gmail.com>
2025-02-15 15:41:02 +0800

    更新設定

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "2025-02-15-test-exam",
+  base: "/2025-02-15-test-exam/",
   plugins: [react()],
 })
